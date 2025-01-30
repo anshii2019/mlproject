594a7e347125885b73156a18988e85335f2a6eab
Anshika Arya <anshikaarya042@gmail.com>
2025-01-30 16:34:00 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
+
+
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
@@ -21,7 +23,7 @@ setup(
     name='mlproject',
     version='0.0.1',
     author='Anshika', 
-    author_email='anshikaaryao42@gmail.com',
+    author_email='anshikaarya042@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 
